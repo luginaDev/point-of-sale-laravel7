570d9220a3b8f1f44eca120647396e7fe3c6d9b4
luginaDev <luginaDev@gmail.com>
2020-07-17 12:50:28 +0700

    templating

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
+    <p class="text-muted text-center text-md-left">Copyright © 2020 <a href="https://www.nobleui.com/" target="_blank">NobleUI</a>. All rights reserved</p>
+    <p class="text-muted text-center text-md-left mb-0 d-none d-md-block">Handcrafted With <i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i></p>
+</footer>
+</div>
+</div>
+<!-- base js -->
+<script src="{{ asset('assets/js/app.js') }}"></script>
+<script src="{{ asset('assets/plugins/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<!-- end base js -->
+<!-- plugin js -->
+<script src="{{ asset('assets/plugins/chartjs/Chart.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/jquery.flot/jquery.flot.js') }}"></script>
+<script src="{{ asset('assets/plugins/jquery.flot/jquery.flot.resize.js') }}"></script>
+<script src="{{ asset('assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/progressbar-js/progressbar.min.js') }}"></script>
+<script src="{{ asset('assets/js/template.js') }}"></script>
+<script src="{{ asset('assets/js/dashboard.js') }}"></script>
+<script src="{{ asset('assets/js/datepicker.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>POS</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- CSRF Token -->
+  <meta name="_token" content="r0jm9Q0QbgITp1DcHcjKFqH5SWUojoCavOOjQpPs">
+  
+  <link rel="shortcut icon" href="favicon.ico">
+
+  <!-- plugin css -->
+  <link href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/plugins/flag-icon-css/css/flag-icon.min.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" />
+  <!-- end plugin css -->
+
+    <link href="{{ asset('assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet" />
+
+  <!-- common css -->
+  <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" />
+  <!-- end common css -->
+
+  </head>
